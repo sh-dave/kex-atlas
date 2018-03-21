c7534971cd22b32cf99f966969539ef97981bc50
David Klein <david.klein@psmtec.de>
2018-03-21 09:18:55 +0100

    move examples to separate repo

--- Example/compiletime-jsonarray/khafile.js
+++
@@ -1,6 +0,0 @@
-let project = new Project('compiletime');
-project.addAssets('../common/Assets/**');
-project.localLibraryPath = '../common/Libraries';
-project.addSources('../..');
-project.addSources('src');
-resolve(project);
--- Example/compiletime-starling-xml/khafile.js
+++
@@ -1,7 +0,0 @@
-let project = new Project('compiletime');
-project.addAssets('../common/Assets/**');
-project.localLibraryPath = '../common/Libraries';
-project.addLibrary('haxe-format-starling');
-project.addSources('../..');
-project.addSources('src');
-resolve(project);
