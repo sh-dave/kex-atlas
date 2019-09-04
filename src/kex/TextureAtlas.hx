b6e0f87507bc5d03751a1c3933aed8b9b0f8d5e7
David Klein <david.klein@psmtec.de>
2019-09-04 08:45:07 +0200

    inline

--- src/kex/TextureAtlas.hx
+++ src/kex/TextureAtlas.hx
@@ -2,7 +2,7 @@ package kex;
 
 @:forward
 abstract TextureAtlas(Map<String, SubTexture>) {
-	@:from public static function ofMap( m: Map<String, SubTexture> )
+	@:from public static inline function ofMap( m: Map<String, SubTexture> )
 		return new TextureAtlas(m);
 
 	inline function new( m: Map<String, SubTexture> )
